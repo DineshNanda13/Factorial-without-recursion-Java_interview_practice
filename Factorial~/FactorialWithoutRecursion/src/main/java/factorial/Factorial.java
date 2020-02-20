83a0f88cb72a6eb68b3b20ae56bfbb3fa0ee2a82
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-20 17:38:52 -0500

    factorial without recursion

---
+++ Factorial~/FactorialWithoutRecursion/src/main/java/factorial/Factorial.java
@@ -0,0 +1,25 @@
+package factorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = s.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+        System.out.println("The factorial is "+ fact);
+    }
+}
